23fd2a9d7940114f5817c0b776f9a93709919517
AlokKumar <alok.kumar.23033@iitgoa.ac.in>
2025-04-24 01:09:22 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// Toggle menu on icon click
 function toggleMenu() {
     document.getElementById("nav-links").classList.toggle("active");
   }
+  
+  // Close menu when clicking outside
+  document.addEventListener("click", function(event) {
+    const nav = document.getElementById("nav-links");
+    const toggle = document.querySelector(".menu-toggle");
+  
+    if (!nav.contains(event.target) && !toggle.contains(event.target)) {
+      nav.classList.remove("active");
+    }
+  });
+
+  document.querySelectorAll("#nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+      document.getElementById("nav-links").classList.remove("active");
+    });
+  });
   
